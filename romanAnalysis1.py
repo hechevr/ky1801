39f2092f888c1703816162b0bc1d1558fa093f82
josenzzc <zzczjq96@gmail.com>
2018-10-25 22:26:41 +0800

    update version of roman Analysis

--- romanAnalysis1.py
+++ romanAnalysis1.py
@@ -3,7 +3,11 @@ Purpose:
         Given several notes, return the correct chord
         using ROman numeral to express the chored
 22/10/2018: Add # and flat key of chord, NoteShift, IntCheck, minorModeTable need to change
-23/10/2018 noteInRoman not finished. 
+23/10/2018  NoteOfChord function unfinished. 
+            NoteSHift only calcute flat notes
+            NoteInRoman return flat notes of the give notes
+            Correct express is still unfinished.
+24/10/2018: debuging the correct notes. the other type need to change.sChordType
 '''
 
 import sys
@@ -100,7 +104,7 @@ class SChordType(Enum):
 
 def NoteShift(isFlat,note):
     #used in flat table
-    sIndex=wholeNoteTable[note]
+    sIndex=noteTable[note]
     if isFlat==True:
         sIndex=sIndex-1
     else:
@@ -112,9 +116,7 @@ def NoteShift(isFlat,note):
     for fNote,index in noteTable.items():
         if index==sIndex:
             rNote =fNote
-    if isFlat==False:
-        rNote=FlatToSharp(rNote)
-    return rNote;
+    return rNote
    
 
 def NoteIsFlat(note):
@@ -127,7 +129,9 @@ def NoteIsSharp(note):
         if sNote==note:
             return True
     return False
+
 def NoteOrigin(note):
+    #for C# and C flat, return C
     rNote=note
     for sNote in sharpList:
         if sNote==note:
@@ -142,6 +146,7 @@ def NoteOrigin(note):
 
 #given roman back note
 def NoteInRoman(mType,mKey,ro):
+    #find the calculate note of the chord
     isFlat=False
     isSharp=False
     if NoteIsFlat(mKey) is True:
@@ -152,11 +157,9 @@ def NoteInRoman(mType,mKey,ro):
         if roman==ro:
             roValue=R_N_table[roman]-1
     if isFlat is True or isSharp is True:
-        omKey=NoteOrigin(mKey)
-        tableIndex=flatCheckList.index(omKey)
-    else:
-        tableIndex=flatCheckList.index(mKey)
-        print('table index is ',tableIndex)
+        mKey=NoteOrigin(mKey)
+    tableIndex=flatCheckList.index(mKey)
+       # print('table index is ',tableIndex)
     if mType==ModeType.Minor:
         roKey=minorModeTable[tableIndex][roValue]
     elif mType==ModeType.Major:
@@ -165,100 +168,124 @@ def NoteInRoman(mType,mKey,ro):
         roKey=NoteShift(True,roKey)
     elif isSharp is True:
         roKey=NoteShift(False,roKey)
-        roKey=NoteShift(True,roKey)
-    
-    return roKey,roValue+1
+    #find the correct note for expression
+    chordIndex=tableIndex+roValue
+    if chordIndex>6:
+        chordIndex=chordIndex-7
+
+    chordRoot=flatCheckList[chordIndex]
+    #roKey correct note
+    #chordRoot correct note with wrong express
+    return roKey,roValue+1,chordRoot
 # find note of aug 6 chord
 def FlatToSharp(note):
     if NoteIsFlat(note)==True:
-        for i,j in zip(flatList,sharpList):
-            if i==note:
-                rNote=j
-                break
+        index=flatList.index(note)
+        sIndex=index-1
+        if sIndex==-1:
+            sIndex=6
+        rNote=sharpList[sIndex]
     return rNote
 def SharpToFlat(note):
     if NoteIsSharp(note)==True:
-        for i,j in zip(sharpList,flatList):
-            if i==note:
-                rNote=j
-                break
+        index=sharpList.index(note)
+        sIndex=index+1
+        if sIndex==7:
+            sIndex=0
+        rNote=flatList[rNote]    
     return rNote
 def InterCheck(i,f,s):
     f=NoteOrigin(f)
     if i==3 or i==4:
         sIndex=flatCheckList.index(f)+2
-        if sIndex>7:
-            sIndex=sIndex-7
-        so=flatCheckList[sIndex]
-        #if flatCheckTable[f]<flatCheckTable[so]:
+    if sIndex>6:
+        sIndex=sIndex-7
+   # print('sIndex is',sIndex)
+    oriS=flatCheckList[sIndex]
+#    print('s is ',s)
+    result=NoteResult(oriS,s)
+ #   print('in inter check, result is',result,'origin second note is',oriS)
+    return result,oriS
 
-        #if cs!=
-def SharpCheck(i,f,s):
-    #chek the root of the chord
-   # i: interval f: first note s:second note
-    if NoteIsFlat(s)==False:
-        return s
-    else:
-        f=NoteOrigin(f)
-        os=NoteOrigin(s)
-        if i==3 or i ==4:
-            sIndex=flatCheckTable[f]+2
-            if sIndex>7:
-                sIndex=sIndex-7
-        for note, index in flatCheckTable.items():
-            if index==sIndex:
-                cs=note
-        if cs!=os:
-            s=FlatToSharp(s)
-    return s        
-    #firstNote=root
-    for note in chord.notes:
-        secondNote=note
-        if secondNote==firstNote and NoteIsFlat(note)==False:
-            firstNote=secondNote
-            continue
-        else:
-            index=chord.notes.index(secondNote)
-            if chord.type==ChordType.Major:
-                interval=majorChordTable[index]
-               # if interval.value==2 or interval.value==3:
-
-    #if chord.type==ChordType.Major:
+ 
+def NoteResult(oNote,note):
+    interList=[]
+    absList=[]
+#    print('oNote is',oNote,'note is',note)
+    interList.append(wholeNoteTable[note]-wholeNoteTable[oNote])
+    absList.append(abs(interList[0]))
+    interList.append(wholeNoteTable[note]-wholeNoteTable[oNote]+12)
+    absList.append(abs(interList[1]))
+    interList.append(wholeNoteTable[note]-wholeNoteTable[oNote]-12)
+    absList.append(abs(interList[2]))
+    mValue=min(absList)
+    mIndex=absList.index(mValue)
+ #   print('interList is',interList)
+    inter=interList[mIndex]
+    result=oNote.value
+    #print('inter is',inter)
+    if inter>0:
+        for i in range(inter):
+            result=result+'#'
+    elif inter<0:
+     #   print('result after plus b',result)
+        for i in range(-inter):
+            result=result+'b'
+    
+   # print('result before return',result)
+    return result
 
 def Aug6Chord(mKey,type):
     chordNotes=[]
-    firstNote,rIndex=NoteInRoman(ModeType.Major,mKey,'VI')
-    if NoteIsFlat(firstNote)==False:
-        firstNote=NoteShift(True,firstNote)
-    lastNote,rIndex=NoteInRoman(ModeType.Major,mKey,'IV')
-    lastNote=NoteShift(False,lastNote)
+    output=[]
+    result=NoteInRoman(ModeType.Major,mKey,'VI')
+    firstNote=NoteShift(True,result[0])
+    #print('fn',result[2],'sn',firstNote)
+    output.append(NoteResult(result[2],firstNote))
     chordNotes.append(firstNote)
     chordNotes.append(mKey)
+    output.append(mKey.value)
     if type==SChordType.FA6:
-        thirdNote,rIndex=NoteInRoman(ModeType.Major,mKey,'II')
-        chordNotes.append(thirdNote)
+        result=NoteInRoman(ModeType.Major,mKey,'II')
+        chordNotes.append(result[0])
+        output.append(NoteResult(result[2],result[0]))
     elif type==SChordType.GA6:
-        thirdNote,rIndex=NoteInRoman(ModeType.Major,mKey,'III')
-        if NoteIsFlat(thirdNote)==False:
-            thirdNote=NoteShift(True,thirdNote)
+        result=NoteInRoman(ModeType.Major,mKey,'III')
+        thirdNote=NoteShift(True,result[0])
+        output.append(NoteResult(result[2],thirdNote))
         chordNotes.append(thirdNote)
+    result=NoteInRoman(ModeType.Major,mKey,'IV')
+    lastNote=NoteShift(False,result[0])
     chordNotes.append(lastNote)
-    return chordNotes
+
+    #print('result 2 is ',result[2])
+    output.append(NoteResult(result[2],lastNote))
+
+    return chordNotes,output
+
+def GetOutput(output):
+    re=''
+    for c in output:
+        re=re+c+' '
+    return re
 #find the chord notes
-def NotesOfChord(mode,chord):
-    cFirstNote=chord.root
-    if NoteIsSharp(chord.root)==True:
-        #print('true')
-        firstNote=SharpToFlat(chord.root)
-    else:
-        firstNote=chord.root
+def NotesOfChord(mode,chord,root):
+    #Find the first correct notes.
+    
+    output=[]
+    cNotes=[]
+    output.append(NoteResult(root,chord.root))
+    #print('chord root origin',output,'chord root note is',chord.root)
+    cFirstNote=root
+    cNotes.append(root)
+    firstNote=chord.root
     secondNote=firstNote
     for i in range (chord.num.value-1):
         firstIndex=noteTable[firstNote]
         if(chord.type==ChordType.Major):    
             interval=majorChordTable[i+1].value
             secondIndex=firstIndex+interval
-            print('second index',secondIndex)
+            #print('second index',secondIndex)
         if(chord.type==ChordType.Minor or chord.type==ChordType.Minorf5):
             interval=minorChordTable[i+1].value
             secondIndex=firstIndex+interval
@@ -276,19 +303,19 @@ def NotesOfChord(mode,chord):
         for note in noteTable:
             if noteTable[note]==secondIndex:
                 secondNote=note
-        print('befor sharp check')
-        cSecondNote=SharpCheck(interval,cFirstNote,secondNote)
-        chord.addNotes(cSecondNote)
-        #print('after add note',chord.notes)
+       # print('befor sharp check')
+        result=InterCheck(interval,cFirstNote,secondNote)
+        output.append(result[0])
+        cSecondNote=result[1]
+        cNotes.append(cSecondNote)
+        chord.addNotes(secondNote)
         firstNote=secondNote
         cFirstNote=cSecondNote
-        # print("list ",chord.notes)
-    # check the minor flat 5  
     if chord.type==ChordType.Minorf5:
         print('minorf5')
         chord.notes[2]=NoteShift(True,chord.notes[2])
-        
-   
+        output[2]=NoteResult(cNotes[2],chord.notes[2])
+
     #check the speial type    
     if chord.sType==None:
         print('no special')
@@ -296,24 +323,58 @@ def NotesOfChord(mode,chord):
         if mode.type==ModeType.Major:   
             if chord.sType==SChordType.flat:
                 chord.notes[0]=NoteShift(True,chord.notes[0])
+                output[0]=NoteResult(cNotes[0],chord.notes[0])
                 chord.notes[2]=NoteShift(True,chord.notes[2])
+                output[2]=NoteResult(cNotes[2],chord.notes[2])
             elif chord.sType==SChordType.D:
                 chord.notes[3]=NoteShift(True,chord.notes[3])
+                output[3]=NoteResult(cNotes[3],chord.notes[3])
         elif mode.type==ModeType.Minor:
             if chord.sType==SChordType.flat:
                 chord.notes[0]=NoteShift(True,chord.notes[0])
+                output[0]=NoteResult(cNotes[0],chord.notes[0])
             if chord.sType==SChordType.plus:
                 chord.notes[1]=NoteShift(False,chord.notes[1])
+                output[1]=NoteResult(cNotes[1],chord.notes[1])
             if chord.sType==SChordType.D:
                 chord.notes[0]=NoteShift(False,chord.notes[0])
-              
+                output[0]=NoteResult(cNotes[0],chord.notes[0])
+        outputString=GetOutput(output)
         if chord.sType==SChordType.GA6 or chord.sType==SChordType.FA6 or chord.sType==SChordType.IA6:
-            chord.notes= Aug6Chord(mode.root,chord.sType) 
-        
-    print(chord.notes)
-    return chord.notes
-
+            result= Aug6Chord(mode.root,chord.sType) 
+            chord.notes=result[0]
+            output=result[1]
+    outputString=GetOutput(output)
+    print('chord notes are ',chord.notes)
+    print('output is ',output)
     
+    return output
+
+def NoteInRomanCheck():
+    mType=ModeType.Major
+    for note,index in wholeNoteTable.items():
+        print('chord note is ',note)
+        for roman in R_N_table:
+            print('chord roman is ',roman)
+            chord,num,cChord=NoteInRoman(mType,note,roman)
+            print('chord root is ',chord)
+            print('chord origin is ',cChord)
+        print('\n')
+def ChordToNotesCheck():
+    #inputModeList=['C#','D#','E#','F#','G#','A#','B#']
+    inputModeList=['B']
+    #inputDisList=['I','II','III','IV','V','VI','VII']
+    inputDisList=['DVII']
+    #inputDisList=['I+','bII','IV+','V+','GVI','FVI','ItVI']
+
+    for inputMode in inputModeList:
+        print('input mode is',inputMode)
+        for inputDis in inputDisList:
+            print('input dis is',inputDis)
+            inputNum='2'
+            output=ChordToNote(inputMode,inputDis,inputNum)
+            #print('output is ',output)
+
 def ChordToNote(mRoot,dis,num):
     if(mRoot[0]==mRoot[0].lower()):
         modeType=ModeType.Minor
@@ -324,12 +385,14 @@ def ChordToNote(mRoot,dis,num):
         if note.value==mRoot:
             modeRoot=note
 
+    #print(modeRoot)
     inputMode=Mode(modeRoot,modeType)
-
+    #print('inputMode mode root is',modeRoot,'input mode mode type is ',modeType)
     if dis[-1]=='+':
         inputSType=SChordType.plus
         dis=dis[:-1]
-        print('in plus')
+        #print('in plus')
+        
     elif dis[0]=='b':
         inputSType=SChordType.flat
         dis=dis[1:]
@@ -348,14 +411,19 @@ def ChordToNote(mRoot,dis,num):
     else:
         inputSType=None
 
-    chordRoot,inputRoman=NoteInRoman(modeType,modeRoot,dis)
+    chordRoot,inputRoman,cChordRoot=NoteInRoman(modeType,modeRoot,dis)
+   # print('chord root is ',chordRoot)
+    #print("input ROman is ",inputRoman)
+    #print('cChordRoot is ',cChordRoot)
     if num=='1':
         if(inputMode.type==ModeType.Major):
+            #print('input mode type is major')
             chordType=MajModeTtable[inputRoman]
             inputChord= Chord(chordRoot, ChordNum.triad,chordType,inputSType)
             inputChord.notes = [chordRoot]
             #print('inputchord ',inputChord.notes)
         elif(inputMode.type==ModeType.Minor):
+           # print('input mode type is minor')
             chordType=MinModeTtable[inputRoman]
             inputChord= Chord(chordRoot, ChordNum.triad,chordType,inputSType)
             inputChord.notes = [chordRoot]
@@ -368,9 +436,12 @@ def ChordToNote(mRoot,dis,num):
             chordType=MinModeStable[inputRoman]  
             inputChord= Chord(chordRoot,ChordNum.seventhChord,chordType,inputSType)
             inputChord.notes = [chordRoot]
-            
-    print('input chord type is ',inputChord.type)
-    returnNotes=NotesOfChord(inputMode,inputChord)
+    
+    output=''
+          
+    #print('input chord type is ',inputChord.type)
+    output=NotesOfChord(inputMode,inputChord,cChordRoot)
+    '''
     # result=[]
     result = ""
     for note in returnNotes:
@@ -378,28 +449,29 @@ def ChordToNote(mRoot,dis,num):
         result += note.value
         result += " "
     return result
-
+    '''
+    return output
 # chord table
 majorModeTable=[[Note.C,Note.D,Note.E,Note.F,Note.G,Note.A,Note.B],              
-               [Note.D,Note.E,Note.F_s,Note.G,Note.A,Note.B,Note.C_s],      
-               [Note.E,Note.F_s,Note.G_s,Note.A,Note.B,Note.C_s,Note.D_s],
+               [Note.D,Note.E,Note.G_f,Note.G,Note.A,Note.B,Note.D_f],      
+               [Note.E,Note.G_f,Note.A_f,Note.A,Note.B,Note.D_f,Note.E_f],
                [Note.F,Note.G,Note.A,Note.B_f,Note.C,Note.D,Note.E],             
-               [Note.G,Note.A,Note.B,Note.C,Note.D,Note.E,Note.F_s],
-               [Note.A,Note.B,Note.C_s,Note.D,Note.E,Note.F_s,Note.G_s],
-               [Note.B,Note.C_s,Note.D_s,Note.E,Note.F_s,Note.G_s,Note.A_s]]
+               [Note.G,Note.A,Note.B,Note.C,Note.D,Note.E,Note.G_f],
+               [Note.A,Note.B,Note.D_f,Note.D,Note.E,Note.G_f,Note.A_f],
+               [Note.B,Note.D_f,Note.E_f,Note.E,Note.G_f,Note.A_f,Note.B_f]]
 minorModeTable=[[Note.C,Note.D,Note.E_f,Note.F,Note.G,Note.A_f,Note.B_f],
                 [Note.D,Note.E,Note.F,Note.G,Note.A,Note.B_f,Note.C],
-                [Note.E,Note.F_s,Note.G,Note.A,Note.B,Note.C,Note.D],
+                [Note.E,Note.G_f,Note.G,Note.A,Note.B,Note.C,Note.D],
                 [Note.F,Note.G,Note.A_f,Note.B_f,Note.C,Note.D_f,Note.E_f],
                 [Note.G,Note.A,Note.B_f,Note.C,Note.D,Note.E_f,Note.F],
                 [Note.A,Note.B,Note.C,Note.D,Note.E,Note.F,Note.G],
-                [Note.B,Note.C_s,Note.D,Note.E,Note.F_s,Note.G,Note.A]]
+                [Note.B,Note.D_f,Note.D,Note.E,Note.G_f,Note.G,Note.A]]
 augChordTable={1:M_I.M3,2:M_I.M3,3:M_I.M2}
 majorChordTable={1:M_I.M3,2:M_I.m3,3:M_I.M3,4:M_I.m3,5:M_I.m3,6:M_I.M3}
 minorChordTable={1:M_I.m3,2:M_I.M3,3:M_I.m3,4:M_I.M3,5:M_I.m3,6:M_I.M3}
 dominantCHordTable={1:M_I.M3,2:M_I.m3,3:M_I.m3,4:M_I.M3,5:M_I.m3,6:M_I.M3}
 noteTable={Note.C:1,Note.D_f:2,Note.D:3,Note.E_f:4,Note.E:5,Note.F:6,Note.G_f:7,Note.G:8,Note.A_f:9,Note.A:10,Note.B_f:11,Note.B:12}   
-wholeNoteTable={Note.C_f:12,Note.C:1,Note.C_s:2,Note.D_f:2,Note.D:3,Note.D_s:4,Note.E_f:4,Note.E:5,Note.E_s:6,Note.F_f:5,Note.F:6,Note.F_s:7,Note.G_f:7,Note.G:8,Note.G_s:9,Note.A_f:9,Note.A:10,Note.A_s:11,Note.B_f:11,Note.B:12,Note.B_s:1}   
+wholeNoteTable={Note.C_f:12,Note.C:1,Note.C_s:2,Note.D_f:2,Note.D:3,Note.D_s:4,Note.E_f:4,Note.E:5,Note.E_s:6,Note.F_f:5,Note.F:6,Note.F_s:7,Note.G_f:7,Note.G:8,Note.G_s:9,Note.A_f:9,Note.A:10,Note.A_s:11,Note.B_f:11,Note.B:12,Note.B_s:0}   
 flatCheckTable={Note.C:1,Note.D:2,Note.E:3,Note.F:4,Note.G:5,Note.A:6,Note.B:7} 
 flatCheckList=[Note.C,Note.D,Note.E,Note.F,Note.G,Note.A,Note.B]
 flatList=[Note.C_f,Note.D_f,Note.E_f,Note.F_f,Note.G_f,Note.A_f,Note.B_f]
@@ -435,7 +507,7 @@ def main():
                     if inputNote==note.value:
                         outputChord.addNotes(note)
             outputChord.root=outputChord.notes[0]
-            print(outputChord.notes)
+            #print(outputChord.notes)
             if len(notes)==3:
                 outputChord.num=ChordNum.triad
             elif len(notes)==4:
@@ -451,13 +523,13 @@ def main():
                         indexDif=noteTable[secondNote]+12-noteTable[firstNote]
                     else:
                         indexDif=noteTable[secondNote]-noteTable[firstNote]
-                    print('index dif ',indexDif)
+                   # print('index dif ',indexDif)
                     for musicInterval in M_I:
                         if indexDif==musicInterval.value:
                             outputChord.addInterval(musicInterval)
-                            print('here')
+                            #print('here')
                     firstNote=secondNote
-            print(outputChord.intervals)
+            #print(outputChord.intervals)
             chordTypeCheck=True
             for interval,chordInterval in zip(outputChord.intervals,MajModeTtable):
                 if interval!=chordInterval:
@@ -489,7 +561,7 @@ def main():
             '''
             inputMode=input('in chord to note mode\n input your chord Mode\n')
             
-            
+           
             inputDis=input('input your chord discription\n')
             
             inputNum=input('Choose your chord mode\n1.triad\n 2.seventh chord\n')
@@ -497,13 +569,10 @@ def main():
             print(resultNotes)
             #show the note of the chord
             #inputMode=new Mode(inputRoot,)
-            '''
             
-            for note in Note:
-                print
-                flatNote=NoteShift(True,inputNote)
-                sharpNote=NoteShift(False,inputNote)
-            print ('flat note is ',flatNote,'\nsharp note is ',sharpNote)
+            '''
+            ChordToNotesCheck()
+           # NoteInRomanCheck()
 if __name__ == "__main__":
     main()
 
